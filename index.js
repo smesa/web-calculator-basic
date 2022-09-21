8f7785120409af9cd92316ea8cb903de50599411
Sergio Mesa <mesasergio@gmail.com>
2022-09-20 19:47:57 -0500

    Add operations into calculator

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator__keys');
+const display = document.querySelector('.calculator__display');
+
 
 keys.addEventListener('click', (event) => { 
 
     const key = event.target;
     const action = key.dataset.action;
+    const previousKeyType = calculator.dataset.previousKeyType;
+    const displayedNum = display.textContent;
+    const keyContent = key.textContent;
+
+
+    Array.from(key.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'));
+        
 
     if (!action) {
-        console.log('number key!');
+
+
+        if(displayedNum === '0' || previousKeyType === 'operator') {
+            display.textContent = keyContent;
+        } else {
+            display.textContent = displayedNum + keyContent;
+        }
+
+    }
+
+    if (action === 'decimal') {
+        if (!display.textContent.includes('.')) { 
+            display.textContent = displayedNum + '.';
+        }
     }
 
     if (
@@ -16,19 +39,48 @@ keys.addEventListener('click', (event) => {
         action === 'multiply' ||
         action === 'divide'
     ) {
-        console.log('operator key!');
+        
+        key.classList.add('is-depressed');
+
+        calculator.dataset.previousKeyType = 'operator';
+        calculator.dataset.firstValue = displayedNum;
+        calculator.dataset.operator = action;
+
     }
 
-    if (action === 'decimal') {
-        console.log('decimal key!');
-    }
 
     if (action === 'clear') {
-        console.log('clear key!');
+        display.textContent = '0';
     }
 
     if (action === 'calculate') {
-        console.log('equal key!');
+        const firstValue = calculator.dataset.firstValue;
+        const secondValue = displayedNum;
+        const operator = calculator.dataset.operator;
+
+        display.textContent = calculate(firstValue, operator, secondValue);
     }
 
 })
+
+const calculate = (n1, operator, n2) => { 
+
+    let result = '';
+
+    if (operator === 'add') {
+        result = parseFloat(n1) + parseFloat(n2);
+    }
+    if( operator === 'subtract') {
+        result = parseFloat(n1) - parseFloat(n2);
+    }
+
+    if (operator === 'multiply') {
+        result = parseFloat(n1) * parseFloat(n2);
+    }
+
+    if (operator === 'divide') {
+        result = parseFloat(n1) / parseFloat(n2);
+    }
+
+    return result;
+}
